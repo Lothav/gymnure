f317355d9403ae1e82f0a28e2d92d18d5fe5da7f
Luiz0tavio <luizotavioresende@hotmail.com>
2017-09-15 19:38:43 -0300

    remove medivh

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,8 @@ int main(int argc, char** argv) {
 		Gymnure* gymnure = new Gymnure(windowWidth, windowHeight);
 
 		gymnure->insertData("../../assets/sky.jpg", "../../assets/cube.obj");
-		gymnure->insertData("../../assets/medivh.jpg",
-							"",
-							{
-									{ { -4.0f,  3.0f, 0.0f }, { 0.0f, 1.0f },{ 0.0f, 0.0f, 1.0f } },
-									{ { -4.0f, -3.0f, 0.0f }, { 0.0f, 0.0f },{ 0.0f, 0.0f, 1.0f } },
-									{ {  4.0f,  3.0f, 0.0f }, { 1.0f, 1.0f },{ 0.0f, 0.0f, 1.0f } },
-
-									{ {  4.0f,  3.0f, 0.0f }, { 1.0f, 1.0f },{ 0.0f, 0.0f, 1.0f } },
-									{ { -4.0f, -3.0f, 0.0f }, { 0.0f, 0.0f },{ 0.0f, 0.0f, 1.0f } },
-									{ {  4.0f, -3.0f, 0.0f }, { 1.0f, 0.0f },{ 0.0f, 0.0f, 1.0f } }
-							}
-		);
+		gymnure->insertData("../../assets/chalet.jpg", "../../assets/chalet.obj");
+		
 		while(1) {
 			if(!gymnure->draw()) {
 				break;
