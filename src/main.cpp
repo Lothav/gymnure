100a7428033d6224b220e0e3e24eb445f0b89f8e
Luiz0tavio <luizotavioresende@hotmail.com>
2017-09-15 19:39:10 -0300

    remove medivh

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
 
 		gymnure->insertData("../../assets/sky.jpg", "../../assets/cube.obj");
 		gymnure->insertData("../../assets/chalet.jpg", "../../assets/chalet.obj");
-		
+
 		while(1) {
 			if(!gymnure->draw()) {
 				break;
