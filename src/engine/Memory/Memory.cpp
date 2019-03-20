c566c0b609048df76fdb218714693e08a3516028
Luiz0tavio <me@luizotav.io>
2019-03-20 17:32:40 -0300

    refactor Memory class with Vulkan.hpp

---
+++ src/engine/Memory/Memory.cpp
@@ -0,0 +1,39 @@
+#include "Memory.h"
+
+namespace Engine
+{
+    namespace Memory
+    {
+        static void Memory::copyMemory(vk::DeviceMemory device_memory, const void * object, size_t object_size)
+        {
+            void* _buffer_address = nullptr;
+
+            auto app_data = ApplicationData::data;
+
+            vk::Result res = app_data->device.mapMemory(device_memory, 0, object_size, {}, &_buffer_address);
+            assert(res == vk::Result::eSuccess);
+            memcpy(_buffer_address, object, object_size);
+            vkUnmapMemory(app_data->device, device_memory);
+        }
+
+        static bool Memory::findMemoryType(uint32_t typeBits, const vk::MemoryPropertyFlags& requirements_mask, uint32_t *typeIndex)
+        {
+            auto app_data = ApplicationData::data;
+
+            // Search memtypes to find first index with those properties
+            for (uint32_t i = 0; i < app_data->memory_properties.memoryTypeCount; i++) {
+                if ((typeBits & 1) == 1) {
+                    // Type is available, does it match user properties?
+                    if ((app_data->memory_properties.memoryTypes[i].propertyFlags & requirements_mask) == requirements_mask) {
+                        *typeIndex = i;
+                        return true;
+                    }
+                }
+                typeBits >>= 1;
+            }
+            // No memory types matched, return failure
+            return false;
+        }
+    }
+}
+
--- src/engine/Memory/Memory.h
+++ src/engine/Memory/Memory.h
@@ -5,7 +5,7 @@
 #ifndef OBSIDIAN2D_MEMORY_H
 #define OBSIDIAN2D_MEMORY_H
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan.hpp>
 #include <cassert>
 #include <cstring>
 #include <ApplicationData.hpp>
@@ -18,36 +18,9 @@ namespace Engine
 		{
 		public:
 
-			static void copyMemory(VkDeviceMemory device_memory, const void * object, size_t object_size)
-			{
-				void* _buffer_address = nullptr;
+			static void copyMemory(vk::DeviceMemory device_memory, const void * object, size_t object_size);
 
-				auto app_data = ApplicationData::data;
-
-				VkResult res = vkMapMemory(app_data->device, device_memory, 0, object_size, 0, &_buffer_address);
-				assert(res == VK_SUCCESS);
-				memcpy(_buffer_address, object, object_size);
-				vkUnmapMemory(app_data->device, device_memory);
-			}
-
-			static bool findMemoryType(uint32_t typeBits, VkFlags requirements_mask, uint32_t *typeIndex)
-			{
-				auto app_data = ApplicationData::data;
-
-				// Search memtypes to find first index with those properties
-				for (uint32_t i = 0; i < app_data->memory_properties.memoryTypeCount; i++) {
-					if ((typeBits & 1) == 1) {
-						// Type is available, does it match user properties?
-						if ((app_data->memory_properties.memoryTypes[i].propertyFlags & requirements_mask) == requirements_mask) {
-							*typeIndex = i;
-							return true;
-						}
-					}
-					typeBits >>= 1;
-				}
-				// No memory types matched, return failure
-				return false;
-			}
+			static bool findMemoryType(uint32_t typeBits, const vk::MemoryPropertyFlags& requirements_mask, uint32_t *typeIndex);
 		};
 	}
 }
