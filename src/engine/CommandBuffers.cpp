867cec85c967501274dc89ccaf152ed8af708d49
Luiz0tavio <me@luizotav.io>
2019-03-20 16:46:56 -0300

    refactor CommandBuffers class with Vulkan.hpp

---
+++ src/engine/CommandBuffers.cpp
@@ -0,0 +1,100 @@
+#include "CommandBuffers.h"
+
+namespace Engine
+{
+    CommandBuffers::CommandBuffers(uint swapchain_images_count)
+    {
+        auto app_data = ApplicationData::data;
+
+        vk::CommandBufferAllocateInfo cmd_buff_ai = {};
+        cmd_buff_ai.pNext 			 	= nullptr;
+        cmd_buff_ai.commandPool 	 	= app_data->graphic_command_pool;
+        cmd_buff_ai.level 			 	= vk::CommandBufferLevel::ePrimary;
+        cmd_buff_ai.commandBufferCount  = swapchain_images_count;
+
+        command_buffers_.resize(swapchain_images_count);
+        vk::Result res = app_data->device.allocateCommandBuffers(&cmd_buff_ai, command_buffers_.data());
+        assert(res == vk::Result::eSuccess);
+    }
+
+    CommandBuffers::~CommandBuffers()
+    {
+        auto app_data = ApplicationData::data;
+        app_data->device.freeCommandBuffers(app_data->graphic_command_pool,
+                                            static_cast<uint32_t>(command_buffers_.size()), command_buffers_.data());
+    }
+
+    void CommandBuffers::bindGraphicCommandBuffer(std::vector<Programs::Program*> programs, RenderPass::RenderPass* render_pass, uint32_t width, uint32_t height)
+    {
+        vk::Result res;
+        const vk::DeviceSize offsets[1] = {0};
+
+        auto frame_buffers = render_pass->getFrameBuffers();
+        auto image_count = render_pass->getSwapChain()->getImageCount();
+
+        if(frame_buffers.size() != image_count) assert(false);
+
+        vk::CommandBufferBeginInfo cmd_buf_info = {};
+        cmd_buf_info.pNext 				= nullptr;
+        cmd_buf_info.flags 				= vk::CommandBufferUsageFlagBits::eSimultaneousUse;
+        cmd_buf_info.pInheritanceInfo 	= nullptr;
+
+        vk::ClearValue clear_values[2];
+        std::array<float,4> clear_values_arr = { 0.8f, 0.2f, 0.8f, 1.0f };
+        clear_values[0].color = vk::ClearColorValue(clear_values_arr);
+        clear_values[1].depthStencil = vk::ClearDepthStencilValue{1.0f, 0u};
+
+        vk::RenderPassBeginInfo rp_begin = {};
+        rp_begin.pNext 				= nullptr;
+        rp_begin.renderPass 		= render_pass->getRenderPass();
+        rp_begin.renderArea.offset 	= vk::Offset2D{0, 0};
+        rp_begin.renderArea.extent  = vk::Extent2D{width, height};
+        rp_begin.clearValueCount 	= 2;
+        rp_begin.pClearValues 		= clear_values;
+
+        for(uint32_t i = 0; i < image_count; i++)
+        {
+            res = command_buffers_[i].begin(&cmd_buf_info);
+            assert(res == vk::Result::eSuccess);
+
+            rp_begin.framebuffer = frame_buffers[i];
+            command_buffers_[i].beginRenderPass(&rp_begin, vk::SubpassContents::eInline);
+
+            for(auto& program_obj : programs) {
+
+                for(auto &data : program_obj->data) {
+
+                    Util::Util::initViewport(command_buffers_[i], width, height);
+                    Util::Util::initScissor(command_buffers_[i], width, height);
+
+                    command_buffers_[i].bindPipeline(vk::PipelineBindPoint::eGraphics,
+                                                     program_obj->graphic_pipeline->getPipeline());
+
+                    command_buffers_[i].bindDescriptorSets(vk::PipelineBindPoint::eGraphics,
+                                                           program_obj->descriptor_set->getPipelineLayout(), 0,
+                                                           1, &data->descriptor_set, 0, nullptr);
+
+                    command_buffers_[i].bindVertexBuffers(0, 1, &data->vertex_buffer->getVertexBuffer()->buf, offsets);
+
+                    auto index_count = data->vertex_buffer->getIndexSize();
+                    if(index_count > 0) {
+                        command_buffers_[i].bindIndexBuffer(data->vertex_buffer->getIndexBuffer()->buf, 0, vk::IndexType::eUint32);
+                        command_buffers_[i].drawIndexed(index_count, 1, 0, 0, 1);
+                    } else {
+                        command_buffers_[i].draw(data->vertex_buffer->getVertexSize(), 1, 0, 0);
+                    }
+                }
+            }
+
+            command_buffers_[i].endRenderPass();
+
+            command_buffers_[i].end();
+        }
+    }
+
+    std::vector<vk::CommandBuffer> CommandBuffers::getCommandBuffers()
+    {
+        return command_buffers_;
+    }
+
+}
--- src/engine/CommandBuffers.h
+++ src/engine/CommandBuffers.h
@@ -22,32 +22,13 @@ namespace Engine
 
     private:
 
-        std::vector<VkCommandBuffer> command_buffers_ = {};
+        std::vector<vk::CommandBuffer> command_buffers_ = {};
 
     public:
 
-        explicit CommandBuffers(uint swapchain_images_count)
-        {
-            auto app_data = ApplicationData::data;
+        explicit CommandBuffers(uint swapchain_images_count);
 
-            VkCommandBufferAllocateInfo cmd = {};
-            cmd.sType 				= VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-            cmd.pNext 			 	= nullptr;
-            cmd.commandPool 	 	= app_data->graphic_command_pool;
-            cmd.level 			 	= VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-            cmd.commandBufferCount  = swapchain_images_count;
-
-            command_buffers_.resize(swapchain_images_count);
-            auto res = vkAllocateCommandBuffers(app_data->device, &cmd, command_buffers_.data());
-            assert(res == VK_SUCCESS);
-        }
-
-        ~CommandBuffers()
-        {
-            auto app_data = ApplicationData::data;
-            vkFreeCommandBuffers(app_data->device, app_data->graphic_command_pool,
-                                 static_cast<uint32_t>(command_buffers_.size()), command_buffers_.data());
-        }
+        ~CommandBuffers();
 
         void* operator new(std::size_t size)
         {
@@ -59,80 +40,9 @@ namespace Engine
             // Do not free memory here!
         }
 
-        void bindGraphicCommandBuffer(std::vector<Programs::Program*> programs, RenderPass::RenderPass* render_pass, uint32_t width, uint32_t height)
-        {
-            VkResult res;
-            const VkDeviceSize offsets[1] = {0};
+        void bindGraphicCommandBuffer(std::vector<Programs::Program*> programs, RenderPass::RenderPass* render_pass, uint32_t width, uint32_t height);
 
-            auto frame_buffers = render_pass->getFrameBuffers();
-            auto image_count = render_pass->getSwapChain()->getImageCount();
-
-            if(frame_buffers.size() != image_count) assert(false);
-
-            VkCommandBufferBeginInfo cmd_buf_info = {};
-            cmd_buf_info.sType 							= VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-            cmd_buf_info.pNext 							= nullptr;
-            cmd_buf_info.flags 							= VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
-            cmd_buf_info.pInheritanceInfo 				= nullptr;
-
-            VkClearValue clear_values[2];
-            clear_values[0].color = { { 0.8f, 0.2f, 0.8f, 1.0f } };
-            clear_values[1].depthStencil = { 1.0f, 0 };
-
-            VkRenderPassBeginInfo rp_begin = {};
-            rp_begin.sType 								= VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-            rp_begin.pNext 								= nullptr;
-            rp_begin.renderPass 						= render_pass->getRenderPass();
-            rp_begin.renderArea.offset 				    = {0, 0};
-            rp_begin.renderArea.extent.width 			= width;
-            rp_begin.renderArea.extent.height 			= height;
-            rp_begin.clearValueCount 					= 2;
-            rp_begin.pClearValues 						= clear_values;
-
-            for(uint32_t i = 0; i < image_count; i++)
-            {
-                res = vkBeginCommandBuffer(command_buffers_[i], &cmd_buf_info);
-                assert(res == VK_SUCCESS);
-
-                rp_begin.framebuffer = frame_buffers[i];
-                vkCmdBeginRenderPass(command_buffers_[i], &rp_begin, VK_SUBPASS_CONTENTS_INLINE);
-
-                for(auto& program_obj : programs) {
-
-                    for(auto &data : program_obj->data) {
-
-                        Util::Util::initViewport(command_buffers_[i], width, height);
-                        Util::Util::initScissor(command_buffers_[i], width, height);
-
-                        vkCmdBindPipeline(command_buffers_[i], VK_PIPELINE_BIND_POINT_GRAPHICS, program_obj->graphic_pipeline->getPipeline());
-
-                        vkCmdBindDescriptorSets(command_buffers_[i], VK_PIPELINE_BIND_POINT_GRAPHICS,
-                                                program_obj->descriptor_set->getPipelineLayout(), 0,
-                                                1, &data->descriptor_set, 0, nullptr);
-
-                        vkCmdBindVertexBuffers(command_buffers_[i], 0, 1, &data->vertex_buffer->getVertexBuffer()->buf, offsets);
-
-                        auto index_count = data->vertex_buffer->getIndexSize();
-                        if(index_count > 0) {
-                            vkCmdBindIndexBuffer(command_buffers_[i], data->vertex_buffer->getIndexBuffer()->buf, 0, VK_INDEX_TYPE_UINT32);
-                            vkCmdDrawIndexed(command_buffers_[i], index_count, 1, 0, 0, 1);
-                        } else {
-                            vkCmdDraw(command_buffers_[i], data->vertex_buffer->getVertexSize(), 1, 0, 0);
-                        }
-                    }
-                }
-
-                vkCmdEndRenderPass(command_buffers_[i]);
-
-                res = vkEndCommandBuffer(command_buffers_[i]);
-                assert(res == VK_SUCCESS);
-            }
-        }
-
-        std::vector<VkCommandBuffer> getCommandBuffers()
-        {
-            return command_buffers_;
-        }
+        std::vector<vk::CommandBuffer> getCommandBuffers();
 
     };
 }
