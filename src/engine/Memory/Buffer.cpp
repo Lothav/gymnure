2458adde12e583c1f21b30a4fb83f6c7922d6e8a
Luiz0tavio <me@luizotav.io>
2019-03-20 17:19:50 -0300

    refactor Buffer class with Vulkan.hpp

---
+++ src/engine/Memory/Buffer.cpp
@@ -0,0 +1,48 @@
+#include "Buffer.h"
+
+namespace Engine
+{
+    namespace Memory
+    {
+        Buffer::Buffer(const struct BufferData& buffer_data)
+        {
+            auto app_data = ApplicationData::data;
+            vk::Result res;
+
+            vk::BufferCreateInfo bufferInfo = {};
+            bufferInfo.size 				 = buffer_data.size;
+            bufferInfo.usage 				 = buffer_data.usage;
+            bufferInfo.queueFamilyIndexCount = 0;
+            bufferInfo.pQueueFamilyIndices 	 = nullptr;
+            bufferInfo.sharingMode 			 = vk::SharingMode::eExclusive;
+            bufferInfo.pNext 				 = nullptr;
+
+            res = app_data->device.createBuffer(&bufferInfo, nullptr, &this->buf);
+            assert(res == vk::Result::eSuccess);
+
+            size = static_cast<uint32_t>(buffer_data.size);
+
+            vk::MemoryRequirements memRequirements{};
+            app_data->device.getBufferMemoryRequirements(this->buf, &memRequirements);
+
+            vk::MemoryAllocateInfo allocInfo = {};
+            allocInfo.allocationSize 		 = memRequirements.size;
+            allocInfo.pNext 				 = nullptr;
+
+            auto pass = Memory::findMemoryType(memRequirements.memoryTypeBits, buffer_data.properties, &allocInfo.memoryTypeIndex);
+            assert(pass);
+
+            res = app_data->device.allocateMemory(&allocInfo, nullptr, &this->mem);
+            assert(res == vk::Result::eSuccess);
+
+            app_data->device.bindBufferMemory(this->buf, this->mem, 0);
+        }
+
+        virtual Buffer::~Buffer()
+        {
+            auto device = ApplicationData::data->device;
+            device.destroyBuffer(this->buf, nullptr);
+            device.freeMemory(this->mem, nullptr);
+        }
+    }
+}
--- src/engine/Memory/Buffer.h
+++ src/engine/Memory/Buffer.h
@@ -9,9 +9,9 @@
 
 struct BufferData
 {
-    VkDeviceSize        size;
-    VkBufferUsageFlags  usage;
-    VkFlags             properties;
+    vk::DeviceSize        	size;
+    vk::BufferUsageFlags  	usage;
+	vk::MemoryPropertyFlags properties;
 };
 
 namespace Engine
@@ -23,54 +23,13 @@ namespace Engine
 
 		public:
 
-			VkBuffer 				buf{};
-			VkDeviceMemory 			mem{};
+			vk::Buffer 				buf{};
+			vk::DeviceMemory 		mem{};
 			uint32_t 				size = 0;
 
-            explicit Buffer(const struct BufferData& buffer_data)
-			{
-				auto app_data = ApplicationData::data;
-				VkResult res;
+            explicit Buffer(const struct BufferData& buffer_data);
 
-				VkBufferCreateInfo bufferInfo = {};
-				bufferInfo.sType 				 = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-				bufferInfo.size 				 = buffer_data.size;
-				bufferInfo.usage 				 = buffer_data.usage;
-				bufferInfo.queueFamilyIndexCount = 0;
-				bufferInfo.pQueueFamilyIndices 	 = nullptr;
-				bufferInfo.sharingMode 			 = VK_SHARING_MODE_EXCLUSIVE;
-				bufferInfo.flags 				 = 0;
-				bufferInfo.pNext 				 = nullptr;
-
-				res = vkCreateBuffer(app_data->device, &bufferInfo, nullptr, &this->buf);
-				assert(res == VK_SUCCESS);
-
-				size = buffer_data.size;
-
-				VkMemoryRequirements memRequirements;
-				vkGetBufferMemoryRequirements(app_data->device, this->buf, &memRequirements);
-
-				VkMemoryAllocateInfo allocInfo = {};
-				allocInfo.sType 				 = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-				allocInfo.allocationSize 		 = memRequirements.size;
-				allocInfo.pNext 				 = nullptr;
-
-				auto pass = Memory::findMemoryType(memRequirements.memoryTypeBits, buffer_data.properties, &allocInfo.memoryTypeIndex);
-				assert(pass);
-
-				res = vkAllocateMemory(app_data->device, &allocInfo, nullptr, &this->mem);
-				assert(res == VK_SUCCESS);
-
-				res = vkBindBufferMemory(app_data->device, this->buf, this->mem, 0);
-				assert(res == VK_SUCCESS);
-			}
-
-			virtual ~Buffer()
-			{
-				auto device = ApplicationData::data->device;
-				vkDestroyBuffer(device, this->buf, nullptr);
-				vkFreeMemory(device, this->mem, nullptr);
-			}
+			virtual ~Buffer();
 
 			void* operator new(std::size_t size)
 			{
