453f6d40fa80f7850b9c9ce0cb385576f306f5c6
Luiz0tavio <me@luizotav.io>
2019-03-22 12:06:24 -0300

    improve time spent debug

--- src/engine/Util/Debug.hpp
+++ src/engine/Util/Debug.hpp
@@ -11,30 +11,41 @@
 #include <chrono>
 #include <vulkan/vulkan.hpp>
 
+//#define PRINT_TIME_SPENT
+
 namespace Engine
 {
     #ifdef DEBUG
-    #define DEBUG_CALL(fn)                                                             		                    \
-    {                                                                                               		    \
-        auto start_cf = std::chrono::high_resolution_clock::now();                                  		    \
-        try {                                                                                                   \
-            fn;                                                                                    		        \
-        } catch(std::system_error &e) {                                                                         \
-            std::stringstream error_message;                                                                    \
-            error_message << "Something went wrong."<< std::endl;                                               \
-            error_message << "Error code: " << e.code() << std::endl;                                           \
-            error_message << "Error message: " << e.what() << std::endl;                                        \
-            Engine::Debug::logError(error_message.str());                                                       \
-            exit(-1);                                                                                           \
-        } catch(...) {                                                                                          \
-            Engine::Debug::logError("Unknown error.");                                                          \
-        }                                                                                                       \
-        auto end_cf = std::chrono::high_resolution_clock::now();                                    		    \
-        auto duration_cf = std::chrono::duration<double, std::milli>(end_cf - start_cf).count();    		    \
-        Engine::Debug::logInfo("Took " + std::to_string(duration_cf) + "ms to execute " + std::string(#fn));    \
-    }
+
+        #define TRY_CATCH_BLOCK_FN(fn)                                                                              \
+            try {                                                                                                   \
+                fn;                                                                                    		        \
+            } catch(std::system_error &e) {                                                                         \
+                std::stringstream error_message;                                                                    \
+                error_message << "Something went wrong."<< std::endl;                                               \
+                error_message << "Error code: " << e.code() << std::endl;                                           \
+                error_message << "Error message: " << e.what() << std::endl;                                        \
+                Engine::Debug::logError(error_message.str());                                                       \
+                exit(-1);                                                                                           \
+            } catch(...) {                                                                                          \
+                Engine::Debug::logError("Unknown error.");                                                          \
+            }
+
+        #ifdef PRINT_TIME_SPENT
+            #define DEBUG_CALL(fn)                                                             		                    \
+            {                                                                                               		    \
+                auto start_cf = std::chrono::high_resolution_clock::now();                                  		    \
+                TRY_CATCH_BLOCK_FN(fn)                                                                                  \
+                auto end_cf = std::chrono::high_resolution_clock::now();                                    		    \
+                auto duration_cf = std::chrono::duration<double, std::milli>(end_cf - start_cf).count();    		    \
+                Engine::Debug::logInfo("Took " + std::to_string(duration_cf) + "ms to execute " + std::string(#fn));    \
+            }
+        #else
+            #define DEBUG_CALL(fn) { TRY_CATCH_BLOCK_FN(fn) }
+        #endif
+
     #else
-    #define DEBUG_CALL(fn) fn;
+        #define DEBUG_CALL(fn) fn;
     #endif
 
     class Debug
