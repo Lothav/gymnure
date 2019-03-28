5140cbfae42c82bf9b74a3682e997d36a8833601
Luiz0tavio <me@luizotav.io>
2019-03-28 09:44:22 -0300

    move program impls to base class

--- src/engine/Programs/Default.hpp
+++ src/engine/Programs/Default.hpp
@@ -71,66 +71,6 @@ namespace Engine
 
                 graphic_pipeline->create(descriptor_set->getPipelineLayout(), render_pass, vk::CullModeFlagBits::eNone);
             }
-
-            void addObjData(GymnureObjData&& obj_data) override
-            {
-                auto app_data = ApplicationData::data;
-                auto program_data = std::make_unique<ProgramData>();
-
-                // Load Textures
-                for (std::string& texture_path : obj_data.paths_textures) {
-                    if(!texture_path.empty())
-                        program_data->textures.push_back(
-                            std::make_unique<Descriptors::Texture>(texture_path, transfer_queue_)) ;
-                }
-
-                // Load Vertex
-                program_data->vertex_buffer = std::make_unique<Vertex::VertexBuffer>();
-                if(!obj_data.obj_path.empty())
-                    // Load obj using TinyObjLoader.
-                    program_data->vertex_buffer->loadObjModelVertices(obj_data.obj_path, obj_data.obj_mtl);
-                else
-                    // Empty obj_path. Use triangle as default vertex data.
-                    program_data->vertex_buffer->createPrimitiveTriangle();
-
-                // Create program Descriptor Set.
-                program_data->descriptor_set = descriptor_set->createDescriptorSet();
-
-                data.push_back(std::move(program_data));
-            }
-
-            void prepare() const override
-            {
-                auto app_data = ApplicationData::data;
-
-                std::vector<vk::WriteDescriptorSet, mem::StdAllocator<vk::WriteDescriptorSet>> writes = {};
-
-                for (auto& d : data)
-                {
-                    for (uint32_t i = 0; i < d->textures.size(); ++i)
-                    {
-                        vk::DescriptorImageInfo *texture_info = new vk::DescriptorImageInfo();
-                        texture_info->imageLayout = vk::ImageLayout::eShaderReadOnlyOptimal;
-                        texture_info->imageView   = d->textures[i]->getImageView();
-                        texture_info->sampler 	  = d->textures[i]->getSampler();
-
-                        vk::WriteDescriptorSet write = {};
-                        write.dstArrayElement 	 = 0;
-                        write.descriptorCount 	 = 1;
-                        write.descriptorType 	 = vk::DescriptorType::eCombinedImageSampler;
-                        write.dstBinding 		 = i;
-                        write.pImageInfo 		 = texture_info;
-                        write.dstSet 			 = d->descriptor_set;
-                        writes.push_back(write);
-                    }
-
-                    auto uniform_bind = uniform_buffer_->getWrite(d->descriptor_set);
-                    uniform_bind.dstBinding = static_cast<uint32_t>(d->textures.size());
-                    writes.push_back(uniform_bind);
-                }
-
-                app_data->device.updateDescriptorSets(writes, {});
-            }
         };
     }
 }
